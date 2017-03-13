e43a039cb1f9de7c4a3838d6cab796ed1b2d2da7
alfi25 <alfimaulana2503@gmail.com>
2017-03-13 18:39:09 +0700

    add project

---
+++ application/models/Pegawai_Model.php
@@ -0,0 +1,43 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Pegawai_Model extends CI_Model {
+
+		public function __construct()
+		{
+			parent::__construct();
+			//Do your magic here
+		}	
+
+		public function getDataPegawai()
+		{
+			$query = $this->db->get('pegawai');
+			return $query->result_array();
+		}
+
+		public function getJabatanByPegawai($idPegawai)
+		{
+			$this->db->select('*');
+			$this->db->from('jabatan_pegawai');
+			$this->db->join('pegawai', 'pegawai.id = jabatan_pegawai.fk_pegawai');
+			$this->db->where('pegawai.id', $idPegawai); 
+			$query = $this->db->get();
+			return $query->result_array();
+
+		}
+		public function getAnakByPegawai($idPegawai)
+		{
+			$this->db->select('anak.nama as anakPegawai, anak.tanggalLahir, pegawai.nama');
+			$this->db->from('anak');
+			$this->db->join('pegawai', 'pegawai.id = anak.fk_pegawai');
+			$this->db->where('pegawai.id', $idPegawai); 
+			$query = $this->db->get();
+			return $query->result_array();
+			
+		}
+
+}
+
+/* End of file Pegawai_Model.php */
+/* Location: ./application/models/Pegawai_Model.php */
+ ?>
